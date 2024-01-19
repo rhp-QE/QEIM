72b05b3599279fcd22d1faa92aa7d4abae78088e
RuanHuipeng <2767360276@qq.com>
2024-01-19 23:06:07 +0800

    删除测试代码

--- TestV1/main.go
+++ TestV1/main.go
@@ -11,16 +11,7 @@ func main() {
 	server.Serve()
 }
 
-type AWEIMTestInterface interface {
-	code (a int, b int)
-}
 
-type AWEIMQA struct{
-
-}
-
-
-// TODO
 ///#pramrk private
 func onRequest(ctx context.Context, connection netpoll.Connection) error {
 	arr := make([]byte, 1024)
@@ -35,9 +26,4 @@ func onRequest(ctx context.Context, connection netpoll.Connection) error {
 func prepare(connection netpoll.Connection) (ctx context.Context) {
 	fmt.Println("12312")
 	return nil;
-}
-
-
-func (qa *AWEIMQA) code(a int, b int) {
-	fmt.Println(a + b)
 }
